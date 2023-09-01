89aeeddbceff1b3daf762037202d5ab8a8fd3c27
root <ahmed999nazir@gmail.com>
2023-09-01 16:02:17 -0700

    fix error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+/**
+ * main - it prints the name of the funtion and a new line.
+ * @argv: array holding all the arguments passed into the function
+ * @argc: size of argv[] or number of arguments
+ *
+ * Return: void
+ */
 
-int main(int argc, char *argv[]) {
-    if (argc >= 1) {
-        printf("%s\n", argv[0]);
-    } else {
-        printf("Program name unavailable.\n");
-    }
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
 
-    return 0;
+		printf("%s\n", argv[0]);
+
+	} else
+		printf("Program name unavailable.\n");
+	return (0);
 }
 
