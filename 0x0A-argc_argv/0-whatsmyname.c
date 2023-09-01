4d22c2c7fe526dbd3e2640bd2f2bfd26c3fca5b4
root <ahmed999nazir@gmail.com>
2023-09-01 14:51:09 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-int main (int argc __attribute__((unused)) , char *argv[])
+int main(int argc, char *argv[]) {
+    if (argc >= 1) {
+        printf("%s\n", argv[0]);
+    } else {
+        printf("Program name unavailable.\n");
+    }
 
-{
-
-	printf ("%s\n",argv[0]);
-	return 0;
+    return 0;
 }
+
