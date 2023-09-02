c293f6441141d33dd8e1d7e38b6b0bdfc79a7139
root <ahmed999nazir@gmail.com>
2023-09-01 17:01:11 -0700

    solve multiplication task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function multiplies two numbers and prints the result.
+ *
+ * @argc: The number of arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int result;
+	if (argc != 3)
+		{
+		printf("Error\n");
+		return (0);
+		}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
+
