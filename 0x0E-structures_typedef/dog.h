cf0e5f08797cff34561af2ad472e56b7f25fcb2c
root <ahmed999nazir@gmail.com>
2023-09-12 07:10:42 -0700

    solution one

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A simple structure for a dog.
+ * @name: Name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: A simple stucture of a dog that holds its basic information
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
